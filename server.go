100cbdb048b6933f03e8de3b85f0ec288367a1d8
Genku <mi32n1@hotmail.co.jp>
2018-11-28 15:19:30 +0900

    fix_sql_Sentence

--- server.go
+++ server.go
@@ -25,13 +25,13 @@ func (todo *Todo) BeforeInsert() error {
 
 func main() {
 	db, err := sql.Open("mysql", "user:password@/dbname")
-	if err := nil {
+	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
 	_, err = db.Exec(`
-		CREATE TABLE Todo (
+		CREATE TABLE todo (
 		id VARCHAR(20) PRIMARY KEY,
 		title VARCHAR(20),
 		done NUMERIC(10,2),
@@ -45,7 +45,7 @@ func main() {
 
 	goji.Get("/api/:id", func(c web.C, w http.ResponseWriter, r *http.Request) {
 		var todos []Todo
-		if err := db.Select(&todos, db.Where("id", "=", c.URLParams["id"])); err != nil {
+		if err := db.QueryRow("SELECT * FROM todo WHERE id =", c.URLParams["id"]); err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
@@ -58,16 +58,16 @@ func main() {
 		}
 	})
 
-	goji.Post("/api/:id", func(c web.C, w http.ResponseWritter, r *http.Request) {
+	goji.Post("/api/:id", func(c web.C, w http.ResponseWriter, r *http.Request) {
 		var todos []Todo
-		if err := db.Select(&todos, db.where("id", "=", c.URLParams["id"])); err != nil {
+		if err := db.QueryRow("SELECT * FROM todo WHERE id =", c.URLParams["id"]); err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
 		//db.Selectをどうにかする
 		todo := todos[0]
 		todo.Title = r.PostFromValue("done") == "true"
-		if _, err := db.Update(&todo); err != nil {
+		if _, err := db.Exec("UPDATE todo SET", &todos); err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
@@ -79,15 +79,15 @@ func main() {
 		}
 	})
 
-	goji.Post("/api", func(c web.C, w http.ResponseWritter, r *http.Request) {
+	goji.Post("/api", func(c web.C, w http.ResponseWriter, r *http.Request) {
 		todo := &Todo{
 			Title: r.PostFormValue("title"),
 		}
-		_, err := db.Insert(todo)
+		_, err := db.Exec("INSERT todo VALUES", todo)
 		//db.Insertを略）
 		//というかdb.系を全部　github.com/go-sql-driver/mysql見ながらヤル
 		if err != nil {
-			http.Error(w, err.Errot(), 500)
+			http.Error(w, err.Error(), 500)
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
@@ -98,7 +98,7 @@ func main() {
 	})
 	goji.Get("/api", func(c web.C, w http.ResponseWriter, r *http.Request) {
 		var todos []Todo
-		if err := db.Select(&todos); err != nil {
+		if err := db.QueryRow("SELECT * FROM todo"); err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
